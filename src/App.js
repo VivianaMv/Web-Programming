7cca2e82b29feaed0e4c6099e775484b00b91a3e
VivanaMv <vivianamontoya14@gmail.com>
2024-05-07 10:21:09 -0400

    my first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 > this is h1</h1>
       </header>
+      <main>
+        <p>this is my first para</p>
+      </main>
+      <button className="b1">   My first button     </button>
+      <button className="b2">   My Second button    </button>
+      <button className="b3">     My Third button     </button>
+
+      <table className="tableclass">
+        <tr>
+          <th>Name   </th>
+          <th>Phone  </th>
+        </tr>
+        <tr>
+          <td>Viviana   </td>
+          <td>22222  </td>
+        </tr>
+        <tr>
+        <td>Simon   </td>
+          <td>3333  </td>
+        </tr>
+        <tr>
+        <td>Pargol   </td>
+          <td>44444  </td>
+        </tr>
+        </table>
+
+        <ul className="listul">
+          <li>Teacher1: Pargol</li>
+          <li>Teacher2: Simon</li>
+          <li>Teacher3: Marcos</li>
+          <li>Teacher4: Silvia</li>
+
+        </ul>
     </div>
   );
 }
 
 export default App;
+// any js that y want to use i have to bahe export app
+
+// everithing has to be in the same div, because is a container 
